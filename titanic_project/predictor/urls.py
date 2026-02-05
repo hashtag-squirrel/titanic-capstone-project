2d8ae3a3279b4abc5af97de6665f12e9bf8e66b9
Dhanyakl <109303327+Dhanyakl@users.noreply.github.com>
2026-02-05 15:44:12 +0100

    Added userform, view, and url files

---
+++ titanic_project/predictor/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path,re_path,include
+from predictor import views
+
+app_name='predictor'
+
+urlpatterns = [
+   
+     re_path(r'^userform/', views.userform, name='userform'),
+]
--- titanic_project/predictor/views.py
+++ titanic_project/predictor/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def userform(request):
+    return render(request,'predictor/userform.html')

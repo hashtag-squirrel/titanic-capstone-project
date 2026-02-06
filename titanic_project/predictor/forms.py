24a41433f5860c2715d6b525438895d98eb3d3af
Dhanyakl <109303327+Dhanyakl@users.noreply.github.com>
2026-02-06 16:12:47 +0100

    updated userform fields

--- titanic_project/predictor/forms.py
+++ titanic_project/predictor/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from predictor.models import UserModel
 
 class UserForm(forms.ModelForm):
+
+     gender = forms.ChoiceField(choices=[('', 'select')] + UserModel.gender_choices,required=False)
+     travel_class=forms.ChoiceField(choices=[('', 'select')] + UserModel.travel_class_choices,required=False)
+     port_of_embarkation=forms.ChoiceField(choices=[('', 'select')] + UserModel.embarkation_choices,required=False)
+
      class Meta():
         model=UserModel
-        fields=('full_name','age','gender')
+        fields=('full_name','age','gender','travel_class','port_of_embarkation','cabin')
+        widgets = {
+            'full_name': forms.TextInput(attrs={'placeholder': 'Enter full name','class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'placeholder': 'Enter age','class': 'form-control'}),   
+            'cabin': forms.TextInput(attrs={'class': 'form-control'}),          
+        }
+
+        
+
--- titanic_project/predictor/models.py
+++ titanic_project/predictor/models.py
@@ -7,9 +7,24 @@ class UserModel(models.Model):
        ('female','Female'),
     ]
 
-    full_name= models.CharField(blank=True)
-    age=models.IntegerField(blank=True)
+    travel_class_choices=[
+       ('1','1st'),
+       ('2','2nd'),
+       ('3','3rd')
+    ]
+
+    embarkation_choices=[
+       ('C','Cherbourg'),
+       ('Q','Queenstown'),
+       ('S','Southampton')
+    ]
+
+    full_name= models.CharField(max_length=100,blank=True)
+    age=models.IntegerField(blank=True,null=True)
     gender=models.CharField(max_length=10,choices=gender_choices,blank=True)
+    travel_class=models.CharField(max_length=10,choices=travel_class_choices,blank=True)
+    port_of_embarkation=models.CharField(max_length=10,choices=embarkation_choices,blank=True)
+    cabin=models.CharField(max_length=100,blank=True)
    
 
     def __str__(self):

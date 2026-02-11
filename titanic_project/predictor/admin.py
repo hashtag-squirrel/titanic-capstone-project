deb5a17207a4ed003d65e3015ed5d673a282f583
hashtag-squirrel <alita2000@web.de>
2026-02-11 19:04:49 +0100

    Adjust admin page to show all fields for PredictionModel

--- titanic_project/predictor/admin.py
+++ titanic_project/predictor/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from predictor.models import UserModel, PredictionModel
 
+
+@admin.register(PredictionModel)
+class PredictionModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+    list_display = ('id', 'input_data', 'result', 'probability','created_at')
+
+
 # Register your models here.
 admin.site.register(UserModel)
-admin.site.register(PredictionModel)

1b4c5e41b8cf40e2f6d79da75800fe8f2fd03542
strombergleila-source <strombergleila@gmail.com>
2026-02-16 15:18:49 +0100

    Test creating a Usermodel

--- titanic_project/predictor/tests.py
+++ titanic_project/predictor/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import UserModel, PredictionModel
+from .forms import UserForm
 
 # Create your tests here.
+
+# Test creating a Usermodel
+class UserModelTest(TestCase):
+
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            title='mr',
+            full_name='John Doe',
+            age=30,
+            gender='0',
+            travel_class='1',
+            is_alone=True
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.full_name, 'John Doe')
+        self.assertEqual(self.user.age, 30)
+        self.assertEqual(self.user.gender, '0')
+
